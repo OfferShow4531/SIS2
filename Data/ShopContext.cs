2e8ee6fe0f404fc23230212e77dd9cff143eaa10
Timur Em <offershow4531@gmail.com>
2020-11-16 22:42:07 +0600

    Timur

---
+++ Data/ShopContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SIS222.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SIS222.Data
+{
+    public class ShopContext : DbContext
+    {
+        public ShopContext(DbContextOptions<ShopContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Shop> Shops { get; set; }
+    }
+}
---
+++ Models/Shop.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SIS222.Models
+{
+    public class Shop
+    {
+        public int Id { get; set; }
+        public string MarketName { get; set; }
+        public decimal MarketCost { get; set; }
+        public string MarketType { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime DataBuy { get; set; }
+    }
+}
